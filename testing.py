c183eef6a9ede4dcfa37dfa0b9e8c121b3fbf7df
SotiriaMpont <80161152+SotiriaMpont@users.noreply.github.com>
2023-03-02 16:13:56 +0200

    Create testing.py

---
+++ testing.py
@@ -0,0 +1,15 @@
+import mysql.connector
+import functions
+
+# kalw thn sinartisi pou exw ftia3ei apo to arxeio functions.py (functions.onomasinartisis)
+#functions.InsertDistributor_toDatabase()
+
+#functions.delete_distributor_from_database()
+
+#functions.Dilwsi_Shift()
+
+#functions.Kataxwrise_Aitima_database()
+
+#functions.Rating_from_store() 
+
+functions.Rating_From_Costumer()
